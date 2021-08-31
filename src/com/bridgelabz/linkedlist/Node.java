27e1df72e1cd60fa63aaa7bf13a51622a2dfb347
padmini v <padmini.v@perfios.com>
2021-08-31 14:58:49 +0530

    ADDED: created a linkedlist with 3 nodes

--- src/com/bridgelabz/linkedlist/Node.java
+++ src/com/bridgelabz/linkedlist/Node.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.linkedlist;
 
-public class Node
+public class Node<K>
 {
+	public K key;
+	public Node next;
+	
+	public Node(K key)
+	{
+		this.key=key;
+		this.next=null;
+	}
+	
+	public Node getNext() 
+	{
+		return next;
+	}
+	
+	public void setNext(Node next) 
+	{
+		this.next = next;
+	}
 	
 	
 }
--- src/com/bridgelabz/linkedlist/NodeMain.java
+++ src/com/bridgelabz/linkedlist/NodeMain.java
@@ -2,10 +2,16 @@ package com.bridgelabz.linkedlist;
 
 public class NodeMain 
 {
+
 	public static void main(String[] args) 
 	{
 		
-	
+		Node<Integer> myFirstNode = new Node<>(56);
+		Node<Integer> mySecondNode = new Node<>(30);
+		Node<Integer> myThirdNode = new Node<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		System.out.println(myFirstNode.key + "->"+ myFirstNode.next.key +"->"+ myFirstNode.next.next.key );
 		
 	}
 }
