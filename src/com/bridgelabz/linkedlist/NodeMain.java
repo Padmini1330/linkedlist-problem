9add2f88a67c758a94b9038625820902a8e8795f
padmini v <padmini.v@perfios.com>
2021-08-31 17:07:22 +0530

    REFACTORED: Linkedlist created using Append

--- src/com/bridgelabz/linkedlist/NodeMain.java
+++ src/com/bridgelabz/linkedlist/NodeMain.java
@@ -6,13 +6,13 @@ public class NodeMain
 	public static void main(String[] args) 
 	{
 		
-		Node<Integer> myFirstNode = new Node<>(70);
+		Node<Integer> myFirstNode = new Node<>(56);
 		Node<Integer> mySecondNode = new Node<>(30);
-		Node<Integer> myThirdNode = new Node<>(56);
+		Node<Integer> myThirdNode = new Node<>(70);
 		LinkedList linkedlist=new LinkedList();
-		linkedlist.add(myFirstNode);
-		linkedlist.add(mySecondNode);
-		linkedlist.add(myThirdNode);
+		linkedlist.append(myFirstNode);
+		linkedlist.append(mySecondNode);
+		linkedlist.append(myThirdNode);
 		linkedlist.printNodes();
 		
 	}
