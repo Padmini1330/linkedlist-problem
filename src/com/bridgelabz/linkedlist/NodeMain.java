02dd69a507f20223b97fd1ba2b8553a652ead8f1
padmini v <padmini.v@perfios.com>
2021-08-31 16:49:31 +0530

    REFACTORED: Adds and prints the nodes

--- src/com/bridgelabz/linkedlist/NodeMain.java
+++ src/com/bridgelabz/linkedlist/NodeMain.java
@@ -6,12 +6,14 @@ public class NodeMain
 	public static void main(String[] args) 
 	{
 		
-		Node<Integer> myFirstNode = new Node<>(56);
+		Node<Integer> myFirstNode = new Node<>(70);
 		Node<Integer> mySecondNode = new Node<>(30);
-		Node<Integer> myThirdNode = new Node<>(70);
-		myFirstNode.setNext(mySecondNode);
-		mySecondNode.setNext(myThirdNode);
-		System.out.println(myFirstNode.key + "->"+ myFirstNode.next.key +"->"+ myFirstNode.next.next.key );
+		Node<Integer> myThirdNode = new Node<>(56);
+		LinkedList linkedlist=new LinkedList();
+		linkedlist.add(myFirstNode);
+		linkedlist.add(mySecondNode);
+		linkedlist.add(myThirdNode);
+		linkedlist.printNodes();
 		
 	}
 }
