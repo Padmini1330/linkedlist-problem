24d6683216ab2e2ea101264ac4ccf8a67e2e538e
padmini v <padmini.v@perfios.com>
2021-08-31 22:50:19 +0530

    REFACTORED: Added SortedLinkedList in the main program

--- src/com/bridgelabz/linkedlist/NodeMain.java
+++ src/com/bridgelabz/linkedlist/NodeMain.java
@@ -6,23 +6,18 @@ public class NodeMain
 	public static void main(String[] args) 
 	{
 		
-		Node<Integer> myFirstNode = new Node<>(56);
-		Node<Integer> mySecondNode = new Node<>(30);
-		Node<Integer> myThirdNode = new Node<>(70);
-		Node<Integer> nodeToBeInserted=new Node<>(40);
-		LinkedList linkedlist=new LinkedList();
+		Node<Integer> myFirstNode = new Node(56);
+		Node<Integer> mySecondNode = new Node(30);
+		Node<Integer> myThirdNode = new Node(70);
+		Node<Integer> myFourthNode=new Node(40);
+		SortedLinkedList<Integer> sortedLinkedList= new SortedLinkedList<Integer>();
 		
-		int valueOfPreviousNode = 30;
-		linkedlist.add(myFirstNode);
-		linkedlist.append(myThirdNode);
-		linkedlist.insert(myFirstNode,mySecondNode);
-		linkedlist.insertWithKey(valueOfPreviousNode, nodeToBeInserted);
-		linkedlist.printNodes();
-		
-		INode deletedNode = linkedlist.deleteNodeWithKey(40);
-		System.out.println("The deleted key value is:"+deletedNode.getKey());
-		int sizeOfList = linkedlist.size();
-		linkedlist.printNodes();
-		System.out.println("The size of the linked list is: "+sizeOfList);
+		sortedLinkedList.append(myFirstNode);
+		sortedLinkedList.append(mySecondNode);
+		sortedLinkedList.append(myThirdNode);
+		sortedLinkedList.append(myFourthNode);
+		sortedLinkedList.sort();
+		sortedLinkedList.printNodes();
 	}
 }
+
