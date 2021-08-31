c7aea537a545d870f9bfd5d92b4b442b000416a9
padmini v <padmini.v@perfios.com>
2021-08-31 21:30:04 +0530

    REFACTORED: added insertwithkey method

--- src/com/bridgelabz/linkedlist/NodeMain.java
+++ src/com/bridgelabz/linkedlist/NodeMain.java
@@ -9,20 +9,26 @@ public class NodeMain
 		Node<Integer> myFirstNode = new Node<>(56);
 		Node<Integer> mySecondNode = new Node<>(30);
 		Node<Integer> myThirdNode = new Node<>(70);
+		Node<Integer> nodeToBeInserted=new Node<>(40);
 		LinkedList linkedlist=new LinkedList();
+		
+		int valueOfPreviousNode = 30;
 		linkedlist.add(myFirstNode);
 		linkedlist.append(myThirdNode);
 		linkedlist.insert(myFirstNode,mySecondNode);
-		;
+		linkedlist.insertWithKey(valueOfPreviousNode, nodeToBeInserted);
 		linkedlist.printNodes();
 		
 		INode searchedKey = linkedlist.search(30);
-		if(searchedKey.getKey().equals(30)) {
+		if(searchedKey.getKey().equals(30)) 
+		{
 			System.out.println("Key Found:"+ searchedKey.getKey());
 		}
-		else {
+		else 
+		{
 			System.out.println("Key Not Found");
 		}		
 		
+	
 	}
 }
